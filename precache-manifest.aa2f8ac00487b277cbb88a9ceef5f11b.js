71c323ffd46083903ff96cb9915fefa74c708f0a
Li Wei <liwsuper7@gmail.com>
2020-10-05 18:25:04 +1100

    Updates

---
+++ precache-manifest.aa2f8ac00487b277cbb88a9ceef5f11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfc96969af522c02040802980505e84",
+    "url": "/EHG-Coding-Challenge/index.html"
+  },
+  {
+    "revision": "bdb6e6b66a5b5d541cc1",
+    "url": "/EHG-Coding-Challenge/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2a899b0f29d3524162d9",
+    "url": "/EHG-Coding-Challenge/static/js/2.6b64f50c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EHG-Coding-Challenge/static/js/2.6b64f50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb6e6b66a5b5d541cc1",
+    "url": "/EHG-Coding-Challenge/static/js/main.186c1987.chunk.js"
+  },
+  {
+    "revision": "567dd808b46248803022",
+    "url": "/EHG-Coding-Challenge/static/js/runtime-main.21e51b35.js"
+  }
+]);
